bf42249f6b9c3f30e9a2c82497f8df135c29cc46
kolemasakar <kolemasakar@gmail.com>
2026-08-12 23:35:44 +0300

    Export Phase 5 tools and evidence components

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,41 @@
-from .interfaces import FetchedDocument, ResearchTools, SearchHit
+from .citations import CitationManager
+from .errors import ToolExecutionError
+from .evidence import (
+    EvidenceLinker,
+    EvidenceToolkit,
+    SourceDeduplicator,
+    SourceMetadataExtractor,
+    SourceReliabilityClassifier,
+    SourceValidationResult,
+    SourceValidator,
+    normalize_url,
+)
+from .interfaces import (
+    FetchedDocument,
+    ResearchTools,
+    SearchHit,
+    WebFetchProvider,
+    WebSearchProvider,
+)
+from .web import ResearchToolset, WebFetchTool, WebSearchTool
 
-__all__ = ["FetchedDocument", "ResearchTools", "SearchHit"]
+__all__ = [
+    "CitationManager",
+    "EvidenceLinker",
+    "EvidenceToolkit",
+    "FetchedDocument",
+    "ResearchTools",
+    "ResearchToolset",
+    "SearchHit",
+    "SourceDeduplicator",
+    "SourceMetadataExtractor",
+    "SourceReliabilityClassifier",
+    "SourceValidationResult",
+    "SourceValidator",
+    "ToolExecutionError",
+    "WebFetchProvider",
+    "WebFetchTool",
+    "WebSearchProvider",
+    "WebSearchTool",
+    "normalize_url",
+]
